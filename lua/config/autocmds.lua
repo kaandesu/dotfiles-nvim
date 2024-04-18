275b35b5e1a7fbeffecace16ca3a90117590ea22
kaan <kaandesu00@gmail.com>
2024-04-18 08:32:13 +0200

    deleted unused config on lsp

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -24,21 +24,21 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
-local client = vim.lsp.start_client({
-  name = "kaanlsp",
-  cmd = { "/Users/kaanyilmaz/Desktop/Programming/gits/LSP/main" },
-  filetypes = { "markdown" },
-})
-if not client then
-  vim.notify("there is something wrong with the client")
-end
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "md", "markdown" },
-  callback = function()
-    if not client then
-      vim.notify("there is something wrong with the client")
-      return
-    end
-    vim.lsp.buf_attach_client(0, client)
-  end,
-})
+-- local client = vim.lsp.start_client({
+--   name = "kaanlsp",
+--   cmd = { "/Users/kaanyilmaz/Desktop/Programming/gits/LSP/main" },
+--   filetypes = { "markdown" },
+-- })
+-- if not client then
+--   vim.notify("there is something wrong with the client")
+-- end
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = { "md", "markdown" },
+--   callback = function()
+--     if not client then
+--       vim.notify("there is something wrong with the client")
+--       return
+--     end
+--     vim.lsp.buf_attach_client(0, client)
+--   end,
+-- })
