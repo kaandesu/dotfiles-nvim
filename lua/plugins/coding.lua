59b0f0c67286fc65d1df70b54cd0fa079454a065
kaan <kaandesu00@gmail.com>
2024-10-14 14:59:01 +0200

    removed the stupid emoji thing

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -97,11 +97,11 @@ return {
     },
   },
 
-  {
-    "nvim-cmp",
-    dependencies = { "hrsh7th/cmp-emoji" },
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-    end,
-  },
+  -- {
+  --   "nvim-cmp",
+  --   dependencies = { "hrsh7th/cmp-emoji" },
+  --   opts = function(_, opts)
+  --     table.insert(opts.sources, { name = "emoji" })
+  --   end,
+  -- },
 }
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -42,9 +42,8 @@ return {
   {
     "hrsh7th/nvim-cmp",
     dependencies = { "hrsh7th/cmp-emoji" },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
+    opts = function()
+      -- table.insert(opts.sources, { name = "emoji" })
     end,
   },
 
