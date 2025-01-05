61cff5def2b64af5cfc2612d4eaaa79878523c3a
kaan <kaandesu00@gmail.com>
2025-01-05 10:55:58 +0300

    chore: rfc

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -142,4 +142,14 @@ return {
       require("telescope").load_extension("file_browser")
     end,
   },
+  {
+    "moniquelive/rfc.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("rfc")
+    end,
+  },
 }
